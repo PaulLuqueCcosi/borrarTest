08dd51a7f7c43e4712cf51acdcdb997b396191e4
PaulLuqueCcosi <pluquec@unsa.edu.pe>
2022-11-01 20:46:43 -0500

    envia senal a H2

--- propuestos/propuesto3.c
+++ propuestos/propuesto3.c
@@ -5,71 +5,77 @@
 #include <stdlib.h>
 
 void tratasenial (int);
+void isCreatedHijo2 (int);
 
 void main(){
   pid_t pidHijo1;
   pid_t pidHijo2;
-  pidHijo1 = fork();
+  
+  printf("PADRE: Soy el proceso padre y mi PID es %d\n", getpid());
+  pidHijo2 = fork();
 
-  if (pidHijo1==-1) { // ERROR
-    perror("No se puede lanzar proceso Hijo 1");
+  if (pidHijo2==-1) { // ERROR
+    perror("No se puede lanzar proceso Hijo 2");
     exit(-1);
   }
 
 
-  // hijo 1
-  if (pidHijo1 == 0) { // HIJO
-		       //signal (SIGUSR1, tratasenial);
-		       //while (1)
-		       //  pause();
-    pid_t pidHijo1_1;
-    pidHijo1_1 = fork();
-    if (pidHijo1_1==-1) { // ERROR
+  // hijo 2
+  if (pidHijo2 == 0) { 
+    pid_t pidHijo2_1;
+    pidHijo2_1 = fork();
+
+    if (pidHijo2_1==-1) { // ERROR
       perror("No se puede lanzar proceso Hijo 3\n");
       exit(-1);
     }
     
     // hijo 3
-    if(pidHijo1_1 == 0){ // HIJO
+    if(pidHijo2_1 == 0){ 
       printf("HIJO 3: PID = %d, PPID = %d\n", getpid(), getppid());
-      printf("Soy el proceso hijo 3 y voy a finalizar \n");
+      printf("HIJO 3: Soy el proceso hijo 3 y voy a finalizar \n");
       exit(0);
-    }
-
-    // hijo 1
-    if(pidHijo1_1 >  0){ // PADRE
-      printf("HIJO 1: mi PID es %d y el de mi padre es %d \n", getpid(), getppid());
-      
-      printf("HIJO 1: el pid de hijo 2 es %d \n", pidHijo2);
-
-      printf("Soy el proceso hijo 1 y voy a finalizar \n");
-      sleep(3);
-      exit(0);
-    }
-  }
-
-  // fin hijo 1
-
-  // padre
-  if (pidHijo1 > 0) { // PADRE
-    pidHijo2 = fork();
-
-    if (pidHijo2==-1) { // ERROR
-      perror("No se puede lanzar proceso Hijo 2");
-      exit(-1);
     }
 
     // hijo 2
-    if (pidHijo2 == 0) { // HIJO
-      printf("HIJO 2:  mi PID es %d y el de mi padre es %d \n", getpid(), getppid());
+    if(pidHijo2_1 >  0){ 
+      printf("HIJO 2: mi PID es %d y el de mi padre es %d \n", getpid(), getppid());
 
-      sleep(6);
-      printf("Soy el hijo 2 y vot a finalizar \n");
+      printf("HIJO 2: Soy el proceso hijo 2 y voy a finalizar \n");
+      exit(0);
+    }
+  }
+  
+
+  // padre
+  if (pidHijo2 > 0) { 
+    pidHijo1 = fork();
+
+    if (pidHijo1==-1) { // ERROR
+      perror("No se puede lanzar proceso Hijo 1");
+      exit(-1);
+    }
+
+    // hijo 1
+    if (pidHijo1 == 0) {
+      printf("HIJO 1: mi PID es %d y el de mi padre es %d \n", getpid(), getppid());
+      printf("HIJO 1: el PID del hijo 2 es %d \n", pidHijo2);
+
+      // enviamos la señal al hijo 2 cada 20 seg
+
+      while(1){
+	sleep(20);
+	printf("HIJO 1: envio señal al hijo 2 \n");
+	kill(pidHijo2, SIGUSR1);
+      }
+
+      sleep(2);
+      printf("HIJO 1: Soy el hijo 1 y vot a finalizar \n");
       exit(0);
     }
 
     // padre
-    if (pidHijo2 > 0) { // PADRE
+    if (pidHijo1 > 0) {
       printf("PADRE: esperando a que finalicen los hijos \n");
       wait();
       wait();
@@ -84,4 +90,8 @@ void main(){
 }
 void tratasenial (int nsenial) {
   printf ("Recibida la señal del Padre\n");
+}
+
+void isCreatedHijo2 (int nsenial) {
+  printf ("Hijo 2 creado\n");
 }
